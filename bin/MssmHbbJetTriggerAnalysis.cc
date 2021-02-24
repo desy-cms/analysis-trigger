4a01073c1b558b862430544bfed45bd01be57dc5
leyva-daina <daina.leyva.pernia@desy.de>
2021-02-24 16:30:26 +0100

    macro for calculating jet kinematic trigger efficiency

--- bin/MssmHbbJetTriggerAnalysis.cc
+++ bin/MssmHbbJetTriggerAnalysis.cc
@@ -2,22 +2,24 @@
 
 using namespace analysis;
 using namespace analysis::tools;
-using namespace analysis::JTA;
+using namespace analysis::trigger;
 
 int main(int argc, char ** argv)
 {
     TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
 
-    JetTriggerAnalyser JTA(argc,argv);
+    JetTriggerAnalyser trigger(argc,argv);
+    trigger.jetHistograms("Probes");
+    trigger.jetHistograms("Probes with matching");
 
-    for ( int i = 0 ; i < JTA.nEvents() ; ++i )
+    for ( int i = 0 ; i < trigger.nEvents() ; ++i )
     {
-        if ( ! JTA.event(i)                       )  continue;    // read event, run selection/json
-        if ( ! JTA.triggerSelection()             )  continue;    // trigger
-        if ( ! JTA.preselection()                 )  continue;    // preselection
-        if ( ! JTA.jetCorrections()               )  continue;    // jet corrections
-        if ( ! JTA.jetKTEfirstSelection()         )  continue;    // jet corrections
-        JTA.jetKTEtagandprobeSelection();
+        if ( ! trigger.event(i)                       )  continue;    // read event, run selection/json
+        if ( ! trigger.triggerSelection()             )  continue;    // trigger
+        if ( ! trigger.preselection()                 )  continue;    // preselection
+        if ( ! trigger.jetCorrections()               )  continue;    // jet corrections
+        if ( ! trigger.jetKTEfirstSelection()         )  continue;    // jet corrections
+        trigger.jetKTEtagandprobeSelection();
     }
 } // end main
      
