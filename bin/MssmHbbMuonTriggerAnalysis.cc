d2a6305b6b870f7e9c39d79822bd12e04cc8c56b
leyva-daina <daina.leyva.pernia@desy.de>
2021-05-03 14:41:51 +0200

    macro for MKTE studies

--- bin/MssmHbbMuonTriggerAnalysis.cc
+++ bin/MssmHbbMuonTriggerAnalysis.cc
@@ -1,22 +1,25 @@
 #include "Analysis/Trigger/interface/MuonTriggerAnalyser.h"
+#include "Analysis/Tools/interface/Analysis.h"
+#include <fstream>
+#include <iostream>
 
 using namespace analysis;
 using namespace analysis::tools;
-using namespace analysis::muontrigger;
+using namespace analysis::muontrigger;		
 
 int main(int argc, char ** argv)
 {
-    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
-  
+    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histogram
+
     MuonTriggerAnalyser muontrigger(argc,argv);
 
     for ( int i = 0 ; i < muontrigger.nEvents() ; ++i )
     {
         if ( ! muontrigger.event(i)                       )  continue;     // read event, run selection/json
-        if ( ! muontrigger.triggerSelection()             )  continue;     // trigger
-        if ( ! muontrigger.muonsSelection()          )  continue;    // muon first selection
-        if ( ! muontrigger.muonTagsSelection()            )  continue;      // muon tag selection
-        muontrigger.muonPassandFail_MatchingProbesSelection();
+        if ( ! muontrigger.selectionTrigger()             )  continue;     // trigger
+        if ( ! muontrigger.muonsSelection()               )  continue;     // muon first selection
+        if ( ! muontrigger.muonTagsSelection()            )  continue;     // muon tag selection
+        muontrigger.muonPassandFail_MatchingProbesSelection();  
     }
 } // end main
      
