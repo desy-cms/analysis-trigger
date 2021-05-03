dffc66a3ea9c5124cc58d514282b4507bc9c0b51
leyva-daina <daina.leyva.pernia@desy.de>
2021-05-03 11:16:55 +0200

    macro for JKTE studies

--- bin/MssmHbbJetTriggerAnalysis.cc
+++ bin/MssmHbbJetTriggerAnalysis.cc
@@ -15,7 +15,7 @@ int main(int argc, char ** argv)
     for ( int i = 0 ; i < trigger.nEvents() ; ++i )
     {
         if ( ! trigger.event(i)                       )  continue;    // read event, run selection/json
-        if ( ! trigger.triggerSelection()             )  continue;    // trigger
+        if ( ! trigger.selectionTrigger()             )  continue;    // trigger
         if ( ! trigger.preselection()                 )  continue;    // preselection
         if ( ! trigger.jetCorrections()               )  continue;    // jet corrections
         if ( ! trigger.jetKTEfirstSelection()         )  continue;    // jet corrections
